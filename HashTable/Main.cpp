d83744c7e608cfd50e9661f5c5da850450bdb220
Jack Porter <jackidporter@gmail.com>
2022-03-11 09:13:42 -0800

    (theoretically) finished all hashtable functions

--- HashTable/Main.cpp
+++ HashTable/Main.cpp
@@ -41,8 +41,24 @@ void recPrint(Item* i){
 	}
 }
 
+
+int getLLSize(Item* i){ //recursively find the length of a linked list
+	if (i) {
+		return 1+getLLSize(i->next);
+	}
+	return 0;
+}
+
+bool hasID(Item* i, int ID){
+	if (i){
+		if (i->student->ID = ID) {return true;}
+		return hasID(i->next);
+	}
+	return false;
+}
+
 struct hashTable {
-	int size = 0;
+	int size = 1; //default size is 1
 	Item** items;
 	hashTable(int i){
 		size = i;
@@ -53,13 +69,39 @@ struct hashTable {
 	}
 	void add(Student* s){
 		int index = s->ID % size; //index of this student;
-		//if there are less than 3 items at this index
-		Item* newItem = new Item(items[index], s); //add the new item
-		items[index]
-		//otherwise rehash and check again
+		if (getLLSize(items[index]) < 3) {//if there are less than 3 items at this index
+			if 
+			Item* newItem = new Item(items[index], s); //add the new item
+			newItem->next = items[index];
+			items[index] = newItem;
+		}
+		else { //otherwise rehash and try again
+			rehash();
+			add(s);
+		}
 	}
 	void rehash(){
-		
+		cout << "rehashing" << endl;
+		bool rehashAgain = false; //check if we need to rehash again
+		size = size*2; //double the size of the array
+		Item** oldArray = items; //store the old array
+		items = new Item*[size]; //create new array (double size of old array)
+		for (int i = 0; i < size; i++) { //set all items pointers in the new array to NULL
+			items[i] = NULL;
+		}
+		for (int i = 0; i < size/2; i++){ //for every slot in the old array
+			while(oldArray[i]){//while there are items in this slot		
+				Item* thisItem = oldArray[i]; //pull the top item off that slot and add it to the new array
+				oldArray[i] = thisItem->next;
+				int thisItemIndex = thisItem->student->ID % size;
+				thisItem->next = items[thisItemIndex];
+				items[thisItemIndex] = thisItem;
+				// if there are more than three items in a slot in the new array then rehash again
+				if (getLLSize(items[thisItemIndex]) > 3) rehashAgain = true;
+			}
+		}
+		delete oldArray; //get rid of the old array
+		if (rehashAgain) {rehash();}
 	}
 	void print(){
 		for (int i = 0; i < size; i++){
@@ -86,6 +128,7 @@ int main() {
 		}
 		
 		else if(strcmp(next, "add") == 0){
+			
 		}
 
 		else if(strcmp(next, "delete") == 0){
